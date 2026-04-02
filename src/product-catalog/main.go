77da20ca3c5aad19825b90fdb6168dd2f74621f9
Tarun P <tarunoneplus132@gmail.com>
2026-04-02 17:01:56 +0530

    chore: verify github actions

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -314,6 +314,9 @@ func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error)
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
 	)
+
+
+	
 }
 
 
